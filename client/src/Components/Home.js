14eac3c721cad30ea2fcd59594d4407601f95de3
xsma-L <ismael.traore@epitech.eu>
2022-07-22 18:38:34 +0200

    form back setting...

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -4,8 +4,10 @@ import { useState } from 'react';
 
 function Home(props) {
 
+    const [cityStart, setCityStart] = useState ('Paris');
+    const [cityArrive, setCityArrive] = useState('Londres');
     const [nbAdult, setNbAdult] = useState(0);
-    const [nbChild, setNbChild] = useState(0); 
+    const [nbChild, setNbChild] = useState(0);
 
     return (
         <main className='homepage'>
@@ -13,30 +15,30 @@ function Home(props) {
             <section id='homepage-search'>
                 <div className='book-flight-form'>
                     <h4 className='form-tiile'>Rechercher un vol</h4>
-                    <div className='form-content'>
+                    <form className='form-content'>
                         <div className='from-to'>
                             <div className='from'>
-                                <span>Ville de départ</span>
-                                <input type='text' value='Londres' />  
+                                <label>Ville de départ</label>
+                                <input type='text' value={ cityStart } onChange={e => setCityStart(e.target.value)}/>  
                             </div>
                             <div className='to'>
-                                <span>Ville d'arrivé</span>
-                                <input type='text' value='Paris' />
+                                <label>Ville d'arrivé</label>
+                                <input type='text' value={ cityArrive } onChange={e => setCityArrive(e.target.value)} />
                             </div>
                         </div>
                         <div className='form-date'>
                             <div className='date-from'>
-                                <span>Du</span>
+                                <label>Du</label>
                                 <input type='text' value='23 Avril' className='day-from' />
                             </div>
                             <div className='date-to'>
-                                <span>Au</span>
+                                <label>Au</label>
                                 <input type='text' value='34 Juin' className='day-to' />
                             </div>
                         </div>
                         <div className='form-passenger'>
                             <div className='adult-passenger'>
-                                <span className='adult-title'>Adultes</span>
+                                <label className='adult-title'>Adultes</label>
                                 <div className='adult-nb'>
                                     <span>{ nbAdult }</span>
                                     <div className='increment-decrement'>
@@ -48,7 +50,7 @@ function Home(props) {
                                 </div>
                             </div>
                             <div className='children-passenger'>
-                                <span className='children-title'>Adultes</span>
+                                <label className='children-title'>Adultes</label>
                                 <div className='children-nb'>
                                     <span>{ nbChild }</span>
                                     <div className='increment-decrement'>
@@ -61,7 +63,7 @@ function Home(props) {
                             </div>
                         </div>
                         <button className='search-flights'>Réserver maintenant</button>
-                    </div>
+                    </form>
                     <div className='popular-flight'>
                         <h4 className='popular-title'>Vols populaires</h4>
                         {/* <div className='popular-flight-content'> */}
